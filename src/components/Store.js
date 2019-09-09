faa72c6f737f9da1428b9491dd56fa49562035b3
dhanushrajendra <dhanush.r@shawacademy.com>
2019-09-09 15:49:38 +0530

    added helpers and react routing between components

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
+import { getFunName } from "../helpers";
 
 class Store extends Component {
+  userInput = React.createRef();
+
+  goToStore = event => {
+    event.preventDefault();
+    const storeName = this.userInput.current.defaultValue;
+    this.props.history.push(`/store/${storeName}`);
+  };
+
   render() {
     return (
       <React.Fragment>
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
           <h1>Please enter a Store</h1>
-          <input type="text" required placeholder="Store Name" />
+          <input
+            type="text"
+            ref={this.userInput}
+            required
+            placeholder="Store Name"
+            defaultValue={getFunName()}
+          />
           <button type="submit">Visit Store -></button>
         </form>
       </React.Fragment>
